8b033c472a5060361b94a3d021221f1a93c2d2c1
Mihael Ištvan <mihael.istvan@gmail.com>
2021-10-02 13:03:42 +0200

    updated /dist tasks.js

--- dist/schemas/tasks.js
+++ dist/schemas/tasks.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.putSchema = exports.getSchema = exports.postSchema = void 0;
 var joi_1 = __importDefault(require("joi"));
 exports.postSchema = joi_1.default.object({
-    title: joi_1.default.string().min(3).max(255).required(),
+    title: joi_1.default.string().min(3).max(100).required(),
     dueDate: joi_1.default.string().max(10).required(),
     completed: joi_1.default.boolean().required(),
 });
@@ -17,7 +17,7 @@ exports.getSchema = joi_1.default.object({
     filter_by_title: joi_1.default.string().empty("").default("default value"),
 });
 exports.putSchema = joi_1.default.object({
-    title: joi_1.default.string().max(255),
+    title: joi_1.default.string().max(100),
     dueDate: joi_1.default.string().max(10),
     completed: joi_1.default.boolean(),
 });
